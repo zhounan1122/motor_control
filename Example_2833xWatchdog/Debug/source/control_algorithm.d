01b6e1dfd2022fc7156054fc318d064713791515
zhounan1122 <zhn-lbj@qq.com>
2017-07-24 13:37:35 +0800

    DSP

--- Example_2833xWatchdog/Debug/source/control_algorithm.d
+++ Example_2833xWatchdog/Debug/source/control_algorithm.d
@@ -1,9 +1,9 @@
 # FIXED
 
 source/control_algorithm.obj: ../source/control_algorithm.c
-source/control_algorithm.obj: C:/Users/yuy/Desktop/Example_2833xWatchdog/include/dmctype.h
-source/control_algorithm.obj: C:/Users/yuy/Desktop/Example_2833xWatchdog/include/control_algorithm.h
+source/control_algorithm.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/dmctype.h
+source/control_algorithm.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/control_algorithm.h
 
 ../source/control_algorithm.c: 
-C:/Users/yuy/Desktop/Example_2833xWatchdog/include/dmctype.h: 
-C:/Users/yuy/Desktop/Example_2833xWatchdog/include/control_algorithm.h: 
+C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/dmctype.h: 
+C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/control_algorithm.h: 
