86e8565820d74ce6f1dbbd5a034e91bcd5a247c8
zhounan1122 <zhn-lbj@qq.com>
2017-07-25 17:07:02 +0800

    zn

--- Example_2833xWatchdog/Debug/source/Example_2833xWatchdog.d
+++ Example_2833xWatchdog/Debug/source/Example_2833xWatchdog.d
@@ -1,60 +1,6 @@
 # FIXED
 
 source/Example_2833xWatchdog.obj: ../source/Example_2833xWatchdog.c
-<<<<<<< HEAD
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Device.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Adc.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_DevEmu.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_CpuTimers.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_ECan.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_ECap.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_DMA.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_EPwm.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_EQep.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Gpio.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_I2c.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Mcbsp.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_PieCtrl.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_PieVect.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Spi.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Sci.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_SysCtrl.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_XIntrupt.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Xintf.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Examples.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_GlobalPrototypes.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_EPwm_defines.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Dma_defines.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_I2c_defines.h
-source/Example_2833xWatchdog.obj: C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_DefaultIsr.h
-
-../source/Example_2833xWatchdog.c: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Device.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Adc.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_DevEmu.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_CpuTimers.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_ECan.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_ECap.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_DMA.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_EPwm.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_EQep.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Gpio.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_I2c.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Mcbsp.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_PieCtrl.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_PieVect.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Spi.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Sci.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_SysCtrl.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_XIntrupt.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Xintf.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Examples.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_GlobalPrototypes.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_EPwm_defines.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_Dma_defines.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_I2c_defines.h: 
-C:/Users/yuy/Desktop/motor_control/motor_control/Example_2833xWatchdog/include/DSP2833x_DefaultIsr.h: 
-=======
 source/Example_2833xWatchdog.obj: C:/Users/Administrator/Desktop/motor_control/Example_2833xWatchdog/include/DSP2833x_Device.h
 source/Example_2833xWatchdog.obj: C:/Users/Administrator/Desktop/motor_control/Example_2833xWatchdog/include/DSP2833x_Adc.h
 source/Example_2833xWatchdog.obj: C:/Users/Administrator/Desktop/motor_control/Example_2833xWatchdog/include/DSP2833x_DevEmu.h
@@ -109,4 +55,3 @@ C:/Users/Administrator/Desktop/motor_control/Example_2833xWatchdog/include/DSP28
 C:/Users/Administrator/Desktop/motor_control/Example_2833xWatchdog/include/DSP2833x_I2c_defines.h: 
 C:/Users/Administrator/Desktop/motor_control/Example_2833xWatchdog/include/DSP2833x_DefaultIsr.h: 
 C:/Users/Administrator/Desktop/motor_control/Example_2833xWatchdog/include/control_algorithm.h: 
->>>>>>> refs/remotes/origin/master
